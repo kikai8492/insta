66e9b9aa35305a72c2fb4970f124849c73f99dac
shuto kikai <yysskikai04@icloud.com>
2023-08-05 13:46:13 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  root 'blogs#index'
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
   resources :blogs do
